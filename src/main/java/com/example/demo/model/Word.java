f03a51833de4cd33fcbce6d85fc4173425ee465f
elvira <aelvira312@gmail.com>
2019-05-06 14:52:17 +0600

    add Word-model, Word-repository

--- src/main/java/com/example/demo/model/Word.java
+++ src/main/java/com/example/demo/model/Word.java
@@ -1,7 +1,39 @@
 package com.example.demo.model;
 
 
+import javax.persistence.*;
+
 //@ManyToMany(
 //join Tables Answers and User
+@Entity
+@Table(name = "word")
 public class Word {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String word;
+
+    public Word() {
+    }
+
+    public Word(Long id, String word) {
+        this.id = id;
+        this.word = word;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
 }
