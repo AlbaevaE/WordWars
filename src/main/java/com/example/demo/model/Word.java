f0978dae9c173e8f5718b1be470038db4ebb5abc
elvira <aelvira312@gmail.com>
2019-05-07 14:08:58 +0600

    add some changes

--- src/main/java/com/example/demo/model/Word.java
+++ src/main/java/com/example/demo/model/Word.java
@@ -5,6 +5,7 @@ import javax.persistence.*;
 
 //@ManyToMany(
 //join Tables Answers and User
+
 @Entity
 @Table(name = "word")
 public class Word {
@@ -12,28 +13,4 @@ public class Word {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String word;
-
-    public Word() {
-    }
-
-    public Word(Long id, String word) {
-        this.id = id;
-        this.word = word;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getWord() {
-        return word;
-    }
-
-    public void setWord(String word) {
-        this.word = word;
-    }
 }
